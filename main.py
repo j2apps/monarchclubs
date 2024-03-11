df7347028a9bd32b6f49841786d58d221f706016
Jonah Kim <j2.web.apps@gmail.com>
2024-03-11 10:41:02 -0600

    configured templates and css

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     club_list = get_home_page_club_list()
-    return render_template('templates/home.html', clubs=club_list)
+    return render_template('home.html', clubs=club_list)
 
 @app.route('/club/<id>')
-def club(id):
+def club(id: int):
+    try: id = int(id)
+    except: return '404'
     club_dict = get_club_from_id(id)
-    return render_template('templates/club.html', club=club_dict)
+    return render_template('club.html', club=club_dict)
 
 
 if __name__ == '__main__':
