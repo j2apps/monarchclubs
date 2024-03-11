4ba8b7729530f9a3ac56959e1f37c97db727f4dc
Jonah Kim <j2.web.apps@gmail.com>
2024-03-11 15:46:34 -0600

    configured club_updates to work with google form output

--- modules/club_updates.py
+++ modules/club_updates.py
@@ -1,6 +1,5 @@
 from google_api.google_sheets_api import *
 
-
 def check_club_updates():
     edit_df = get_df_from_sheet('club_edit')
     approved_changes = edit_df[edit_df['is_approved']=='Y']
@@ -16,12 +15,11 @@ def check_club_updates():
 
 def update_club(change, df):
     id = change['club_id']
+    ignored_cols = ['club_id', 'is_approved', 'timestamp', 'email_address']
     for col, value in change.to_dict().items():
-        if str(value).strip() != '' and col != 'club_id' and col != 'is_approved':
+        if str(value).strip() != '' and col not in ignored_cols:
             df.loc[df['club_id']==id, col] = value
     update_sheet_with_df('club', df)
-
-
 
 
 if __name__ == '__main__':
