469a949bd02d5d4a84ef1924eb233bbff31e0a6b
Jonah Kim <j2.web.apps@gmail.com>
2024-03-11 11:27:12 -0600

    added sheet id dict functionality

---
+++ google_api/google_sheets_api.py
@@ -0,0 +1,35 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+import pandas as pd
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+creds = Credentials.from_service_account_file('google_api/sheets_creds.json',scopes=scopes)
+client = gspread.authorize(creds)
+
+id_dict = {"club": ("1EtErKxuI_SCU7avKLKxrFqbVsZrVqyKXEfSiQ3J-tHo","Sheet1")}
+
+
+
+
+
+def get_df_from_sheet(sheet_name: str) -> pd.DataFrame:
+    worksheet = get_worksheet(sheet_name)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+
+def update_sheet_with_df(sheet_name, df):
+    worksheet = get_worksheet(sheet_name)
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+def edit_cell(sheet_name,cell,change):
+    worksheet=get_worksheet(sheet_name)
+    worksheet.update_acell(cell,change)
+
+def get_worksheet(sheet_name: str):
+    sheet_id, sheet_name = id_dict[sheet_name]
+    workbook = client.open_by_key(sheet_id)
+    worksheet = workbook.worksheet(sheet_name)
+    return worksheet
