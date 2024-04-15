755df954c201385376667608e282ce23ff01c978
Jack VanMatre <jackbrian05@gmail.com>
2024-04-15 10:14:55 -0600

    gcal wrapper methods updated

--- modules/cal_methods.py
+++ modules/cal_methods.py
@@ -1,13 +1,6 @@
 from google_api.google_calendar_api import *
-from google_api.google_sheets_api import *
 
-
-def create_gcal_event_from_dict(row_dict):
- 
-    return(create_event(f"{row_dict['club_name']} - {row_dict['event_title']}",row_dict['event_short_description'],format_time(row_dict['event_start_time']),format_time(row_dict['event_end_time']),row_dict['event_location'],row_dict['event_contacts']))
-
-def update_gcal_event_from_dict(row_dict):
-    return(update_event(row_dict['gcal_id'],f"{row_dict['club_name']} - {row_dict['event_title']}",row_dict['event_short_description'],format_time(row_dict['event_start_time']),format_time(row_dict['event_end_time']),row_dict['event_location'],row_dict['event_contacts']))
+import datetime
 
 def format_time(time):
     t = time.split(' ')
@@ -19,3 +12,12 @@ def format_time(time):
         t2+=12
         t3 = str(t2) + f":{t1[1]}"
         return t3
+
+def create_gcal_event_from_dict(row_dict):
+ 
+    return(create_event(f"{row_dict['club_name']} - {row_dict['event_title']}",row_dict['event_short_description'],datetime.datetime.strptime(f"{row_dict['event_date']} {format_time(row_dict['event_start_time'])}",'%m/%d/%y %H:%M:%S'),datetime.datetime.strptime(f"{row_dict['event_date']} {format_time(row_dict['event_end_time'])}",'%m/%d/%y %H:%M:%S'),row_dict['event_location'],row_dict['event_contacts']))
+
+def update_gcal_event_from_dict(row_dict):
+    return(update_event(row_dict['gcal_id'],f"{row_dict['club_name']} - {row_dict['event_title']}",row_dict['event_short_description'],datetime.datetime.strptime(f"{row_dict['event_date']} {format_time(row_dict['event_start_time'])}",'%m/%d/%y %H:%M:%S'),datetime.datetime.strptime(f"{row_dict['event_date']} {format_time(row_dict['event_end_time'])}",'%m/%d/%y %H:%M:%S'),row_dict['event_location'],row_dict['event_contacts']))
+
+
